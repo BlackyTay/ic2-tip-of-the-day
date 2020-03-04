841d10dc7e41df7c0eef37235ddae60eb1f5539f
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-04 15:50:22 +0800

    Add service to retrieve data from json

--- src/app/services/tips.service.ts
+++ src/app/services/tips.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class TipsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getTips() {
+    return this.httpClient.get("/assets/data/tips.json");
+  }
 }
